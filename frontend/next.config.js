54a3570970cd598f7b1f4da8512a613e68ffb84d
crazydj8 <akshataryan8@gmail.com>
2025-01-25 22:02:29 +0530

    updated api port

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    API_URL: process.env.API_URL || 'http://127.0.0.1:5000',
+    API_URL: process.env.API_URL || 'http://127.0.0.1:8080',
   },
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://127.0.0.1:5000/:path*', // Proxy to Backend
+        destination: 'http://127.0.0.1:8080/:path*', // Proxy to Backend
       },
     ];
   },
