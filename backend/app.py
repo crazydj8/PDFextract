47de962a21dfb33a2a01f1834e4cfd6e7f121afb
crazydj8 <akshataryan8@gmail.com>
2025-01-25 21:44:43 +0530

    updated port

--- backend/app.py
+++ backend/app.py
@@ -63,5 +63,5 @@ def get_metadata():
     return jsonify(metadata=result['metadata']), 200
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port, debug=True)
